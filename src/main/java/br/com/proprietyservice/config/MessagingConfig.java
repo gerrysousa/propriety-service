70c3e36d1fd8a96ba0f86dc2e116fc9d80726b6c
Gerry Sousa <gerry@ciandt.com>
2021-05-11 07:28:28 -0300

    Refactor, now the queue is on rabbit cloud insted of a local rabbit MQ

--- src/main/java/br/com/proprietyservice/config/MessagingConfig.java
+++ src/main/java/br/com/proprietyservice/config/MessagingConfig.java
@@ -1,6 +1,10 @@
 package br.com.proprietyservice.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
